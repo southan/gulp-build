92a94f8fa7ad11138a860cbe23bc78286e0c3383
Alex Southan <southan@users.noreply.github.com>
2016-07-12 11:00:41 +0100

    Refactor.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+/* jshint node: true */
 "use strict";
 
-var util = getLazy({
-	del    : "del",
-	merge  : "merge-stream",
-	assign : "object-assign",
-	util   : "gulp-util",
-	plumber: "gulp-plumber",
-	notify : "node-notifier"
+var getLazy = require( "./lib/lazy" );
+
+/**
+ * Our own little helper.
+ *
+ * @type {Object}
+ */
+var utils = getLazy({
+	del       : "del",
+	chalk     : "chalk",
+	log       : "fancy-log",
+	merge     : "merge-stream",
+	notifier  : "node-notifier",
+	path      : "path",
+	plumber   : "gulp-plumber",
+	prettyTime: "pretty-hrtime"
 });
 
 /**
@@ -19,82 +29,26 @@ var util = getLazy({
  * @param   args.dest  Destination of stream
  * @returns stream
  */
-function gulpBuild( args ) {
+function Build( args ) {
 	var src = this.src( args.files, args.base ? { base: args.base } : null );
 
-	if ( args.onError )
-		src = src.pipe( util.plumber( args.onError ) );
+	if ( args.onError ) {
+		src = src.pipe( utils.plumber( args.onError ) );
+	}
 
 	if ( args.pipes ) {
-		args.pipes.forEach( function ( pipe ) {
-			if ( pipe )
+		args.pipes.forEach(function ( pipe ) {
+			if ( pipe ) {
 				src = src.pipe( pipe );
-		});
-	}
-
-	if ( args.dest )
-		src = src.pipe( this.dest( args.dest ) );
-
-	return src;
-}
-
-/**
- * Get plugins object fallback from project package.json
- *
- * @returns object
- */
-function getPlugins() {
-	try {
-		var plugins = {};
-		var project = require( process.cwd() + "/package.json" );
-		var modules = util.assign( {}, project.devDependencies, project.dependencies );
-
-		Object.keys( modules ).forEach( function ( module ) {
-			var nameProp = module.replace( /-/g, "_" );
-			var namePropShort = nameProp.replace( "gulp_", "" );
-
-			// Gulp plugins get priority e.g. "jshint" will reference gulp-jshint over jshint
-			if ( typeof plugins[ namePropShort ] === "undefined" || nameProp !== namePropShort  )
-				plugins[ namePropShort ] = module;
-		});
-
-		return plugins;
-
-	} catch ( error ) {
-		return {};
-	}
-}
-
-/**
- * Get a modules object for lazy require.
- *
- * @param   modules
- * @returns object
- */
-function getLazy( modules ) {
-	var lazy = {};
-
-	Object.keys( modules ).forEach( function ( name ) {
-		var module = modules[ name ];
-
-		Object.defineProperty( lazy, name, {
-			configurable: true,
-			enumerable  : true,
-			get         : function () {
-				var value = require( module );
-
-				Object.defineProperty( this, name, {
-					configurable: false,
-					writable:     false,
-					value:        value
-				});
-
-				return value;
 			}
 		});
-	});
+	}
 
-	return lazy;
+	if ( args.dest ) {
+		src = src.pipe( this.dest( args.dest ) );
+	}
+
+	return src;
 }
 
 /**
@@ -102,61 +56,65 @@ function getLazy( modules ) {
  *
  * @param error
  */
-function onError( error ) {
-	util.notify.notify({
+function Error( error ) {
+	utils.log.error( error.message );
+	utils.notifier.notify({
 		title  : error.plugin || "Error",
 		message: error.message
 	});
-
 	this.emit( "end" );
 }
 
-module.exports = function ( gulp, options ) {
-	var browserSync;
-	var build = gulpBuild.bind( gulp );
-	var tasks = options.tasks || options;
-	var opts  = {
-		onError: options.onError || onError,
-		dest   : options.dest || "public",
-		base   : options.base || "build"
-	};
+module.exports = function ( gulp, tasks ) {
+	/**
+	 * Global task defaults.
+	 *
+	 * @type {Object}
+	 */
+	var defaults = Object.assign({ onError: Error }, tasks.defaults );
+	delete tasks.defaults;
 
-	opts.plugins = getLazy( options.plugins || getPlugins() );
-	opts.package = util.assign({
-		dest : "_package",
-		args : {
-			minify: true
-		},
-		files: [
-			"**",
-			"!.*",
-			"!*.md",
-			"!*.log",
-			"!gulpfile.js",
-			"!package.json",
-			"!browserSync.json",
-			"!node_modules{,/**}",
-			"!" + opts.base + "{,/**}",
-			"*.htaccess"
-		]
-	}, options.package );
+	/**
+	 * Lazy-loaded plugins. Defaults to parsing the package.json of the current
+	 * project.
+	 *
+	 * @type {Object}
+	 */
+	var plugins = getLazy( tasks.plugins || require( "./lib/plugins" ) );
+	delete tasks.plugins;
 
-	Object.keys( tasks ).forEach( function ( name ) {
-		var task = tasks[ name ];
+	/**
+	 * Build() instance bound to gulp.
+	 *
+	 * @type {Function}
+	 */
+	var build = Build.bind( gulp );
 
-		if ( typeof task.onError === "undefined" )
-			task.onError = opts.onError;
-		if ( typeof task.merge === "undefined" )
-			task.merge = true;
-		if ( typeof task.base === "undefined" )
-			task.base = opts.base;
-		if ( typeof task.dest === "undefined" )
-			task.dest = opts.dest;
+	/**
+	 * Process arguments.
+	 *
+	 * @type {Object}
+	 */
+	var argv = require( "minimist" )( process.argv.slice( 2 ) );
+
+	/**
+	 * Package arguments.
+	 *
+	 * @type {Object}
+	 */
+	var pkg = Object.assign( {}, tasks.package );
+	delete tasks.package;
+
+	/**
+	 * Register tasks.
+	 */
+	Object.keys( tasks ).forEach(function ( name ) {
+		var task = Object.assign( tasks[ name ], defaults );
 
 		gulp.task( name, function () {
 			return build({
 				onError: task.onError,
-				pipes  : task.pipes && task.pipes.call( opts.plugins, util.assign( {}, util.util.env ) ),
+				pipes  : task.pipes && task.pipes.call( plugins, Object.assign( {}, argv ) ),
 				files  : task.files,
 				base   : task.base,
 				dest   : task.dest
@@ -164,46 +122,77 @@ module.exports = function ( gulp, options ) {
 		});
 	});
 
+	/**
+	 * Clean
+	 */
 	gulp.task( "clean", function () {
-		return util.del([ "_package/**", opts.dest + "/**" ]);
-	});
-
-	gulp.task( "build", [ "clean" ], function () {
-		var merged = util.merge(),
-			args   = util.assign( {}, this.seq.slice( -1 )[0] === "package" ? opts.package.args : {}, util.util.env );
-
-		Object.keys( tasks ).forEach( function ( name ) {
-			var task = tasks[ name ];
-
-			merged.add(
-				build({
-					onError: task.onError,
-					files  : task.files,
-					pipes  : task.pipes && task.pipes.call( opts.plugins, args ),
-					base   : task.base,
-					dest   : task.dest
-				})
-			);
+		var paths = Object.keys( tasks ).map(function ( name ) {
+			return tasks[ name ].dest;
 		});
 
-		return merged;
+		return utils.del( paths.concat([ "_package/**" ]) );
 	});
 
-	gulp.task( "watch", function () {
+	/**
+	 * Build
+	 */
+	gulp.task( "build", [ "clean" ], function () {
+		console.log( plugins );
+
+		var args = Object.assign(
+			{},
+			this.seq.slice( -1 )[0] === "package" ? pkg.args : {},
+			argv
+		);
+
+		var srcs = Object.keys( tasks ).map( function ( name ) {
+			var task = tasks[ name ];
+
+			return build({
+				onError: task.onError,
+				files  : task.files,
+				pipes  : task.pipes && task.pipes.call( this, args ),
+				base   : task.base,
+				dest   : task.dest
+			});
+		}, plugins );
+
+		return utils.merge( srcs );
+	});
+
+	/**
+	 * Watch
+	 */
+	gulp.task( "watch", [ "build" ], function () {
+		var browserSync;
+
+		try {
+			browserSync = require( process.cwd() + "/browserSync.json" );
+			browserSync = require( "browser-sync" ).create();
+			browserSync.init( browserSync );
+		} catch ( e ) {
+			browserSync = null;
+		}
+
 		Object.keys( tasks ).forEach( function ( name ) {
 			var task = tasks[ name ];
 
 			gulp.watch( task.watch || task.files ).on( "change", function ( file ) {
+				var filename = utils.chalk.magenta( utils.path.basename( file.path ) );
+				var time     = process.hrtime();
+
+				utils.log( "Starting", filename, "..." );
+
 				var stream = build({
 					onError: task.onError,
-					files  : task.merge ? task.files : file.path,
-					pipes  : task.pipes && task.pipes.call( opts.plugins, util.util.env ),
+					files  : task.doOne ? file.path : task.files,
+					pipes  : task.pipes && task.pipes.call( plugins, argv ),
 					base   : task.base,
 					dest   : task.dest
 				});
 
 				return stream.on( "end", function () {
-					util.util.log( "Finished", util.util.colors.magenta( name ) );
+					utils.log( "Finished", filename, "after", utils.chalk.magenta( utils.prettyTime( process.hrtime( time ) ) ) );
 
 					if ( browserSync && task.sync )
 						browserSync.reload( task.sync );
@@ -212,26 +201,34 @@ module.exports = function ( gulp, options ) {
 		});
 	});
 
+	/**
+	 * Package
+	 */
 	gulp.task( "package", [ "build" ], function () {
-		return build( opts.package );
+		var files = [
+			"**",
+			"!.*",
+			"!*.md",
+			"!*.log",
+			"!gulpfile.js",
+			"!package.json",
+			"!browserSync.json",
+			"!_package{,/**}",
+			"!node_modules{,/**}"
+		];
+
+		if ( Array.isArray( pkg.files ) )
+			files = files.concat( pkg.files );
+		else if ( pkg.files )
+			files.push( pkg.files );
+
+		return build({
+			files: files,
+			dest : "_package"
+		});
 	});
 
-	gulp.task( "browserSync", function () {
-		try {
-			var config = require( process.cwd() + "/browserSync.json" );
-		} catch ( error ) {
-			return;
-		}
-
-		browserSync = require( "browser-sync" ).create();
-		browserSync.init( config );
-	});
-
-	gulp.task( "default", [
-		"build",
-		"watch",
-		"browserSync"
-	]);
+	gulp.task( "default", [ "watch" ] );
 
 	return build;
 };
